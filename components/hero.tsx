44de4c34fccd95ba66a42d8169bb3846dd4c386e
s.toda <donateluro@gmail.com>
2025-02-23 12:09:34 +0900

    fix:imageOnを追加しheroの画像を出し分ける機能作成

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,8 +1,15 @@
-export function Hero() {
+interface HeroProps {
+    title: string;
+    subtitle: string;
+    imageOn?: boolean
+}
+
+export function Hero({ title, subtitle, imageOn=false }: HeroProps) {
     return (
         <>
-            <h1>CUBE</h1>
-            <p>アウトプットするサイト</p>
+            <h1>{title}</h1>
+            <p>{subtitle}</p>
+            {imageOn && <figure> [画像] </figure>}
         </>
     );
 }
