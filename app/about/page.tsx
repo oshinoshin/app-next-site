bcf26ad820768b20c0886aff923e1251c9fb1c94
s.toda <donateluro@gmail.com>
2025-02-23 12:08:48 +0900

    fix:aboutページの追加

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import { Layout } from "@/components/layout";
+import { Hero } from "@/components/hero";
+
+export default function About() {
+    return (
+        <Layout>
+            <Hero title="About" subtitle="アバウトページです" />
+        </Layout>
+    );
+}
