b52cac67eafb5791b74ea4009c5ce08b45ee8227
s.toda <donateluro@gmail.com>
2025-02-23 12:09:05 +0900

    fix:ブログページの追加

---
+++ app/blog/page.tsx
@@ -0,0 +1,10 @@
+import { Layout } from "@/components/layout";
+import { Hero } from "@/components/hero";
+
+export default function Blog() {
+    return (
+        <Layout>
+            <Hero title="Blog" subtitle="ブログページ" />
+        </Layout>
+    );
+}
