6a5176ce9228bab22c9dc6fc8082864e3ab42c8b
s.toda <donateluro@gmail.com>
2025-03-02 13:21:03 +0900

    fix:footerのstyle適用

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,13 @@
-import { Logo } from '@/components/logo';
+import { Logo } from "@/components/logo";
+import styles from "@/styles/footer.module.css";
 
 export function Footer() {
     return (
-        <footer>
-            FOOTER
-            <Logo />
+        <footer className={styles.wrapeer}>
+            <div className={styles.flexContainer}>
+                <Logo />
+                [ソーシャル]
+            </div>
         </footer>
     );
 }
